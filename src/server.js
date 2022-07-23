aae2f1f6c3cb1cb010e6c41fa888dace62c0d0f1
Jeong Chong In <chongin12@naver.com>
2022-07-23 20:26:25 +0900

    edit folder struct

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import express from "express"
 const app = express();
 
 app.set("view engine", "pug")
-app.set("views", __dirname + "\\public\\views")
+app.set("views", __dirname + "\\views")
 app.use("/public", express.static(__dirname + "\\public"))
 
 app.get("/", (req, res) => res.render("home"))
